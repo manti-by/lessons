18fa84ce16fc67ab76c797aab2bfa6c646b097c7
Alexander Chaika <manti.by@gmail.com>
2022-05-20 21:29:34 +0300

    Add messages

--- lesson_03/classwork.py
+++ lesson_03/classwork.py
@@ -1,27 +1,35 @@
+print("Exercise #1")
 x = 17 / 2 % 2 * 3
 print(x)
 
+print("Exercise #2")
 x = 17 / 2 % (2 * 3)
 print(x)
 
 
+print("Exercise #3")
 olga = 33
 maxim = 29
 roman = 36
 
 print(olga + maxim + roman)
+
+print("Exercise #4")
 print((olga + maxim + roman) / 3)
 
 
+print("Exercise #5")
 my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
 my_set = set(my_list)
 print(len(my_list) - len(my_set))
 
 
+print("Exercise #6")
 my_slice = my_list[3:6]
 print(my_slice[::-1])
 
 
+print("Exercise #7")
 size = 13
 print([size * 4, size ** 2, size * size, (size ** 2 * 2) ** 0.5])
 
