52f1b39cc7866f915a58d4b13726547539e1642f
Alexander Chaika <manti.by@gmail.com>
2022-06-15 17:01:49 +0300

    Add homework #9

---
+++ lesson_09/homework.py
@@ -0,0 +1,26 @@
+"""
+Создать список фигур и в цикле подсчитать и вывести площади всех фигур на экран.
+"""
+
+from figures import Point, Circle, Triangle, Square
+
+
+def main():
+    a = Point(3, 7)
+    circle = Circle(a, 3)
+
+    a = Point(3, 7)
+    b = Point(5, 2)
+    square = Square(a, b)
+
+    a = Point(1, 1)
+    b = Point(7, 2)
+    c = Point(5, 5)
+    triangle = Triangle(a, b, c)
+
+    for figure in [circle, square, triangle]:
+        print(figure.__class__.__name__, figure.perimeter(), figure.area())
+
+
+if __name__ == "__main__":
+    main()
