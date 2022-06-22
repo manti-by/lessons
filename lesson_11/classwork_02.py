fd865eea6ee94ec79a69ba70762f809a949059cc
Alexander Chaika <manti.by@gmail.com>
2022-06-22 18:03:47 +0300

    Update classwork #11

--- lesson_11/classwork_02.py
+++ lesson_11/classwork_02.py
@@ -6,15 +6,29 @@
 import sqlite3
 
 
-def select_user(from_age: int, to_age: int):
+def select_user_by_name(name: str) -> list:
     with sqlite3.connect("my_database.sqlite3") as session:
         cursor = session.cursor()
         cursor.execute(
             """
             SELECT *
             FROM user
-            WHERE age >= ? AND  age <= ?
-            LIMIT 2, 3;
+            WHERE firstname = ?;
+            """,
+            (name,)
+        )
+        session.commit()
+        return cursor.fetchall()
+
+
+def select_user_by_age(from_age: int, to_age: int) -> list:
+    with sqlite3.connect("my_database.sqlite3") as session:
+        cursor = session.cursor()
+        cursor.execute(
+            """
+            SELECT *
+            FROM user
+            WHERE age >= ? AND  age <= ?;
             """,
             (from_age, to_age)
         )
@@ -23,4 +37,14 @@ def select_user(from_age: int, to_age: int):
 
 
 if __name__ == "__main__":
-    print(select_user(18, 50))
+    while True:
+        choice = input("Выполнить поиск по возрасту [В] или имени [И]:")
+        if choice == "В":
+            start = input("Введите минимальный возраст:")
+            stop = input("Введите максимальный возраст:")
+            print(select_user_by_age(int(start), int(stop)))
+        elif choice == "И":
+            search = input("Введите имя:")
+            print(select_user_by_name(search))
+        else:
+            exit()
