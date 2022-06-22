4705ff280be4ed466491092b3a9e22b9f59b0aef
Alexander Chaika <manti.by@gmail.com>
2022-06-22 17:56:33 +0300

    Add more examples

--- lesson_12/main.py
+++ lesson_12/main.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import sessionmaker
 
-from models import Base, User
+from models import Base, User, Address, Profile
 from utils import setup_db_engine, create_database_if_not_exists
 
 
@@ -14,4 +14,22 @@ if __name__ == "__main__":
 
     user = User(email="test@test.com", password="password")
     session.add(user)
+    session.commit()
+
+    session.query(User).filter_by(email="test@test.com")
+    session.query(User).filter_by(email="test@test.com").first()
+
+    session.query(User).filter_by(email="test@test.com").update({"email": "updated@test.com"})
+    session.query(User).filter_by(email="test@test.com").first().update({"email": "updated@test.com"})
+
+    session.query(User).filter(User.email == "test@test.com").first()
+    session.query(Profile).filter(Profile.age >= 15)
+
+    user = session.query(User).filter_by(email="test@test.com").first()
+    user.email = "updated@test.com"
+    session.add(user)
+    session.commit()
+
+    address = Address(user=user, city="Test", address="Test 123")
+    session.add(address)
     session.commit()
