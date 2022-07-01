e437e30025d5d25783fae08a874546f1b6553f84
Alexander Chaika <manti.by@gmail.com>
2022-07-01 19:29:32 +0300

    Flask + SQLAlchemy

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -1,6 +1,10 @@
 import logging
 
-from flask import Flask, request
+from flask import Flask, request, Response
+from sqlalchemy.orm import sessionmaker
+
+from utils import create_database_if_not_exists, setup_db_engine
+from models import Base, User, Profile, Address, Product, Purchase
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,34 +19,28 @@ def dict_to_str(data: dict) -> str:
     return ", ".join(result)
 
 
-def dict_to_str_gen(data: dict) -> str:
-    return ", ".join([
-        f"{key}: {value}" for key, value in data.items()
-    ])
-
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
+    return Response("Index page", status=400)
+
+
+@app.route("/user/", methods=["GET", "POST"])
+def user():
     if request.method == "POST":
-        logger.info(
-            dict_to_str(
-                request.form.to_dict()
-            )
-        )
-        return request.form.to_dict()
-
-    logger.info(
-        dict_to_str_gen(
-            request.args.to_dict()
-        )
-    )
-    return request.args.to_dict()
-
-
-@app.route("/test/", methods=["GET"])
-def test():
-    return "Test URL"
+        new_user = User(email=request.form.get("email"), password=request.form.get("password"))
+        app.current_session.add(new_user)
+        app.current_session.commit()
+        return Response(str(new_user), status=201)
+    data = map(str, app.current_session.query(User).all())
+    return Response("\n".join(data))
 
 
 if __name__ == "__main__":
+    engine = setup_db_engine()
+    create_database_if_not_exists(engine=engine)
+
+    Base.metadata.create_all(engine)
+    CurrentSession = sessionmaker(bind=engine)
+    app.current_session = CurrentSession()
+
     app.run()
