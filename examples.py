e834379722b096330ae5e37ffe00e9cfc662a24d
Alexaner Chaika <manti.by@gmail.com>
2022-07-13 16:36:25 +0300

    Update formatting

--- examples.py
+++ examples.py
@@ -1,20 +1,18 @@
 from Shop.purchase.print_result_to_console import print_result_to_console
 
+
 session.query(Product).filter_by(id=id).update({"name": name})
 session.query(Product).filter_by(id=id).update({"price": price})
 session.query(Product).filter_by(id=id).update({"number": number})
 session.query(Product).filter_by(id=id).update({"comment": comment})
 
 
+
 def get_prod_list(session: Session) -> Product:
     return session.query(Product.id, Product.name, Product.price, Product.ammount, Product.comment).all()
-
-
 def update_prod(session: Session, id: int, name: str, price: int) -> Product:
     session.query(Product).filter_by(id=id).update({"name": name, "price": price})
     session.commit()
-
-
 def del_prod(session: Session, id: int) -> Product:
     session.query(Product).filter_by(id=id).delete()
     session.commit()
